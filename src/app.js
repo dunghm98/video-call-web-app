1d0ae8ed0c4e29f701d9f3eefab4e8374b779414
Dung manh <dunghm98@gmail.com>
2020-01-30 22:35:01 +0700

    optimize function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 const openStream = require('./openStream');
+const Peer = require('simple-peer');
+const $ = require('jquery');
+const playvideo = require('./playVideo');
 
-openStream();
+openStream(function(stream){
+    playvideo(stream,'localStream');
+    const p = new Peer({
+        initiator: location.hash === '#1',
+        trickle: false,
+        stream:stream
+    });
+    p.on('signal', token => {
+        $('#txtMySignal').val(JSON.stringify(token));
+    });
+    $('#btnConnect').click(() => {
+        const friendSignal = JSON.parse($('#txtMyFriendSignal').val());
+        p.signal(friendSignal);
+    });
+    p.on('stream', friendStream => playvideo(friendStream,'friendStream'));
+});
--- src/openStream.js
+++ src/openStream.js
@@ -1,38 +1,19 @@
-const playvideo = require('./playVideo');
-const Peer = require('simple-peer');
-const $ = require('jquery');
-
-function openStream(){
+function openStream(callback){
     navigator.getWebcam = (navigator.getUserMedia || navigator.webKitGetUserMedia || navigator.moxGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({  audio: false, video: true })
-            .then(function (stream) {
-                console.log(stream);
-                console.log('run');
-                playvideo(stream,'localStream');
-                const p = new Peer({
-                    initiator: location.hash === '#1',
-                    trickle: false,
-                    stream:stream
-                });
-                p.on('signal', token => {
-                    $('#txtMySignal').val(JSON.stringify(token));
-                });
-                p.on('stream', friendStream => playvideo(friendStream,'friendStream'));
-
-                $('#btnConnect').click(() => {
-                    const friendSignal = JSON.parse($('#txtMyFriendSignal').val());
-                    p.signal(friendSignal);
-                });
+            .then( stream => {
+                callback(stream);
+                
             })
             .catch(function (e) { console.log(e.name + ": " + e.message); });
     }
     else {
-        navigator.getWebcam({ audio: true, video: true },
-            function (stream) {
-                playvideo(stream,'localStream');
+        navigator.getWebcam({ audio: false, video: true },
+            stream => {
+                callback(stream);
             },
-            function () { console.log("Web cam is not accessible."); });
+             () => console.log("Web cam is not accessible."));
     }
 }
 
